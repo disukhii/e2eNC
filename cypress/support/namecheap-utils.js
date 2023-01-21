598f17e187be3e8a775e5185f47efc65f3bd77ab
Dmytro Sukhii <63877784+disukhii@users.noreply.github.com>
2023-01-21 14:39:58 +0200

    [TESTS] Automation of Authorization page. Not registered user
    [TESTS] Automation of Authorization page. Not registered user

--- cypress/support/namecheap-utils.js
+++ cypress/support/namecheap-utils.js
@@ -4,5 +4,18 @@ const user = {
     email: 'ssls.automation+666@gmail.com',
     password: '123456'
 }
+const errorPopups = {
+    incorrectEmailOrPassword: 'Uh oh! Email or password is incorrect'
+}
+function randomString(length, email = false) {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    if (email) result += '@gmail.com'
+    return result;
+}
 
-module.exports = { user, url }
+module.exports = { user, url, randomString, errorPopups }
